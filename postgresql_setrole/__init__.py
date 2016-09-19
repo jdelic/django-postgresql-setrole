1c83259e579dd3548290be0398974e83cb2fa9d7
Jonas Maurus <jonas-github@maurus.net>
2016-09-19 23:29:54 +0200

    fix options case and make set_role case optional

--- postgresql_setrole/__init__.py
+++ postgresql_setrole/__init__.py
@@ -1,4 +1,6 @@
 # -* encoding: utf-8 *-
+import warnings
+
 from django.apps import AppConfig
 from django.db.backends.postgresql.base import DatabaseWrapper as PostgreSQLDatabaseWrapper
 from django.db.backends.signals import connection_created
@@ -7,10 +9,19 @@ from typing import Any, Type
 
 def setrole_connection(*, sender: Type[PostgreSQLDatabaseWrapper],
                        connection: PostgreSQLDatabaseWrapper, **kwargs: Any) -> None:
-    if "options" in connection.settings_dict and "set_role" in connection.settings_dict["options"]:
-        connection.cursor().execute("SET ROLE %s",
-                                    (sender.settings_dict["options"]["set_role"],))
+    if "OPTIONS" in connection.settings_dict:
+        role = None
+        if "set_role" in connection.settings_dict["OPTIONS"]:
+            role = connection.settings_dict["OPTIONS"]["set_role"]
+        elif "SET_ROLE" in connection.settings_dict["OPTIONS"]:
+            role = connection.settings_dict["OPTIONS"]["SET_ROLE"]
 
+        if role:
+            connection.cursor().execute("SET ROLE %s", (role,))
+        else:
+            warnings.warn("postgresql_setrole app is installed, but no SET_ROLE vaule is in database OPTIONS")
+    else:
+        warnings.warn("postgresql_setrole app is installed, but no OPTIONS dict is in database settings")
 
 class DjangoPostgreSQLSetRoleApp(AppConfig):
     name = "postgresql_setrole"
