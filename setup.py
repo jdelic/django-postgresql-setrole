7c5f60316e2142ce87d290ee1051ca70b232a497
Jonas Maurus <jonas-github@maurus.net>
2016-09-19 22:32:03 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -* encoding: utf-8 *-
+from setuptools import setup
+
+setup(
+    name="django-postgresql-setrole",
+    version="1.0.0",
+    packages=["postgresql_setrole"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX",
+    ],
+    author="Jonas Maurus (@jdelic)",
+    author_email="jdelic-postgresql-setrole@gopythongo.com",
+    maintainer="GoPythonGo.com",
+    maintainer_email="info@gopythongo.com",
+    description="Build shippable virtualenvs",
+)
