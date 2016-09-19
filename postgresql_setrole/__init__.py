2323fdf9ae54ff7b94ccc9d37b452178f8cf6270
Jonas Maurus <jonas-github@maurus.net>
2016-09-19 23:22:55 +0200

    fix up the signal kwargs, take generic kwargs, bump version. Now it works

--- postgresql_setrole/__init__.py
+++ postgresql_setrole/__init__.py
@@ -2,10 +2,12 @@
 from django.apps import AppConfig
 from django.db.backends.postgresql.base import DatabaseWrapper as PostgreSQLDatabaseWrapper
 from django.db.backends.signals import connection_created
+from typing import Any, Type
 
 
-def setrole_connection(sender: PostgreSQLDatabaseWrapper, connection: 'psycopg2._psycopg.connection') -> None:
-    if "options" in sender.settings_dict and "set_role" in sender.settings_dict["options"]:
+def setrole_connection(*, sender: Type[PostgreSQLDatabaseWrapper],
+                       connection: PostgreSQLDatabaseWrapper, **kwargs: Any) -> None:
+    if "options" in connection.settings_dict and "set_role" in connection.settings_dict["options"]:
         connection.cursor().execute("SET ROLE %s",
                                     (sender.settings_dict["options"]["set_role"],))
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="django-postgresql-setrole",
-    version="1.0.1",
+    version="1.0.3",
     packages=["postgresql_setrole"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
