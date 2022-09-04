b5ea4405ed786fe8857a812cda218c2fd446d5a5
Jonas Maurus <jonas-github@maurus.net>
2022-09-04 21:49:11 +0200

    bump version for release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except IOError:
 
 setup(
     name="django-postgresql-setrole",
-    version="1.0.11",
+    version="1.0.12",
     packages=["postgresql_setrole"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
